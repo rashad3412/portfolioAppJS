5f29e50ae7fd68497d6fe194003df9f8c4519258
Rashad Edwards <rashadedwards@macbook.local>
2024-08-25 18:21:32 -0400

    added and updated new feats to project

--- JsFiles/index.js
+++ JsFiles/index.js
@@ -3,6 +3,7 @@ const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".link");
 const content = document.querySelector(".main-content-section");
 const container = document.querySelector(".container");
+const projectPage = document.getElementById("#projects-section");
 
 // function to toggle between active class for hamburger menu
 function toggleNavMenu() {
@@ -42,4 +43,48 @@ function addCircles(numberOfCircles) {
 
 addCircles(7);
 
-// function to remove circles
+document.addEventListener("DOMContentLoaded", () => {
+  const projectComponent = document.getElementById("project-component");
+
+  // Function to load project.html content
+  function loadProjectComponent() {
+    fetch("project.html")
+      .then((response) => response.text())
+      .then((data) => {
+        projectComponent.innerHTML = data;
+      })
+      .catch((error) => console.log("Error loading Project Component:", error));
+  }
+
+  // Check screen size on initial load
+  if (window.innerWidth >= 1024) {
+    loadProjectComponent();
+  }
+
+  // Check screen size on window resize
+  window.addEventListener("resize", () => {
+    if (window.innerWidth >= 1024) {
+      loadProjectComponent();
+      projectComponent.style.display = "block";
+      projectSectionHeader.style.display = "none";
+    } else {
+      projectComponent.style.display = "none";
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const projectLink = document.getElementById("project-link");
+
+  const projectSection = document.getElementById("project-component");
+
+  projectLink.addEventListener("click", (e) => {
+    if (window.innerWidth >= 1024) {
+      e.preventDefault();
+      projectSection.scrollIntoView({
+        behavior: "auto",
+      });
+    } else {
+    }
+  });
+});
